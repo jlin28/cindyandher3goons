70ebd0de2edd20b3b9f7a0e6b9a2a6cd09a11e9c
Joyce Lin <joycel78@nycstudents.net>
2026-05-12 18:03:56 -0400

    added temp node server setup code

---
+++ app/app.js
@@ -0,0 +1,23 @@
+const WebSocket = require('ws');
+
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+const ws = new WebSocket.Server({
+  port: PORT,
+  host: HOST
+});
+
+ws.on('error', console.error);
+
+ws.on('connection', function connect(client) {
+  console.log("client has connected");
+
+  client.on('message', message => {
+    console.log(`Received '${message}' from client`);
+  })
+
+  client.on('close', () => {
+    console.log("Client has disconnected");
+  });
+});

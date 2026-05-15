cbcfb45f2121f5748fb5f6394945d6034297d91a
Cindy <cindyl125@nycstudents.net>
2026-05-15 10:49:24 -0400

    cl update

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,9 @@ db = sqlite3.connect(DB_FILE, check_same_thread=False)
 c = db.cursor()
 c.execute("CREATE TABLE IF NOT EXISTS user(username TEXT, password TEXT, hp INTEGER NOT NULL, stamina INTEGER NOT NULL, item1 TEXT, item2 TEXT, item3 TEXT, item4 TEXT, item5 TEXT, item6 TEXT, item1Count INTEGER, item2Count INTEGER, item3Count INTEGER, item4Count INTEGER, item5Count INTEGER, item6Count INTEGER);")
 c.execute("CREATE TABLE IF NOT EXISTS item(name TEXT, desc TEXT NOT NULL, image TEXT NOT NULL, maxCount INTEGER NOT NULL)")
+c.execute("INSERT into item VALUES ('carrot', 'an orange vegetable', '', 1)")
+c.execute("INSERT into item VALUES ('button', 'a circle to make your bestie dapper', '', 3)")
+c.execute("INSERT into item VALUES ('snowball', 'a bundle of joy', '', 99)")
 c.execute("CREATE TABLE IF NOT EXISTS encyclopedia(item TEXT, userThatFound TEXT)")
 c.execute("CREATE TABLE IF NOT EXISTS npc(name TEXT, dialogue TEXT NOT NULL, completedQuestUsers TEXT)")
 db.close()
@@ -75,15 +78,6 @@ def register():
 def game():
     return render_template('game.html')
 
-@app.route("/exit", methods=["GET", "POST"])
-def exit():
-    return render_template('exit.html')
-
-@app.route("/credit", methods=["GET", "POST"])
-def credit():
-    return render_template('credit.html')
-
-
 if __name__ == "__main__":
-    app.debug = True
+    app.debug = False
     app.run()

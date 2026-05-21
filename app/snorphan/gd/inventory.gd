7e6fd09ffe6f4b2a3bec4c897ac17bce11ad41a3
Joyce Lin <jlin60@stuy.edu>
2026-05-21 11:48:09 -0400

    input maps instead of event

--- app/snorphan/gd/inventory.gd
+++ app/snorphan/gd/inventory.gd
@@ -9,14 +9,14 @@ func _ready() -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	var inventory_index = -1
 	# M3 Wheel Down --> inv goes from 1 to 6
-	if(event == InputEventMouseButton and event.pressed and Input.get_mouse_button_mask() == MOUSE_BUTTON_MASK_RIGHT):
+	if(Input.is_action_pressed("right_click")):
 		inventory_index += 1
 		print("right button clik click")
 	
-	# Code is wrong down there will fix when I get home
-	# M3 Wheel Up --> inv goes from 6 to 1
-	#if(event == InputEventMouseButton and event.pressed and MouseButton.get_button_index() == MOUSE_BUTTON_WHEEL_UP):
-	#	
-	# Right clicks will deselect
-	#if(event == InputEventMouseButton and event.pressed and MouseButton.get_button_index() == MOUSE_BUTTON_RIGHT):
-	#	#deselect code here
+	 #Code is wrong down there will fix when I get home
+	 #M3 Wheel Up --> inv goes from 6 to 1
+	#if(Input.is_action_pressed("mouse_up")):
+		#
+	 #Right clicks will deselect
+	#if(Input.is_action_pressed("mouse_down")):
+		##deselect code here

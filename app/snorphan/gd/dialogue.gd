d1dea0bd420787ccdb76fa4a6ac5b79ed3221a62
Joyce Lin <jlin60@stuy.edu>
2026-05-28 03:57:09 -0400

    ???

--- app/snorphan/gd/dialogue.gd
+++ app/snorphan/gd/dialogue.gd
@@ -66,6 +66,7 @@ func play_dialogue():
 			player.active_quests.append(current_npc)
 			player.completion_status.append(0)
 			MultiplayerClient.add_quest(current_npc)
+			await get_tree().create_timer(0.3).timeout
 			quests_cont.change_quests.emit()
 		
 	dialogue_box.text = current_dialogue[current_dialogue_line].dialogue

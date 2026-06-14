a871d62b207bbeeba03a208b516025ef87157a7b
Joyce Lin <joycel78@nycstudents.net>
2026-06-14 01:38:55 -0400

    working on encyclopedia ui

--- app/snorphan/tscn/close_encyclopedia.gd
+++ app/snorphan/tscn/close_encyclopedia.gd
@@ -1,11 +1,11 @@
 extends Button
 
-@onready var settings_cont := %settings
+@onready var encyclopedia_cont := %encyclopedia
 @onready var main_ui := %default
 @onready var player := get_tree().get_first_node_in_group('player')
 
 
 func _pressed() -> void:
-	settings_cont.visible = false
+	encyclopedia_cont.visible = false
 	main_ui.visible = true
 	player.can_move = true

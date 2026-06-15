fd19fc3c4afef556fa898742df7cf3191d0cc390
Joyce Lin <jlin60@stuy.edu>
2026-06-15 03:36:00 -0400

    >?>>

--- app/__init__.py
+++ app/__init__.py
@@ -1216,11 +1216,11 @@ def game():
             return jsonify( { 'request': 'handled' })
 
 		if body.get('type') == 'remove_quest':
-            npc = body.get('npc')
-            user = body.get('user')
-            remove_quest(npc, user)
-
-            return jsonify( { 'request': 'handled' })
+			npc = body.get('npc')
+			user = body.get('user')
+			remove_quest(npc, user)
+			
+			return jsonify( { 'request': 'handled' })
 
         if body.get('type') == 'complete_quest':
             npc = body.get('npc')

74d987300e54ee332de004ea5d54960c99799e55
Jeff <jou60@stuy.edu>
2026-06-14 23:47:16 -0400

    town chief item cap

--- app/snorphan/gd/dialogue.gd
+++ app/snorphan/gd/dialogue.gd
@@ -43,7 +43,7 @@ func play_dialogue():
 			var current_amount = 1.0;
 			if item_to_be_taken != "house": # For Town Chief's quest
 				current_amount = player.current_items[item_to_be_taken]
-			if player.full_inventory and current_amount - amount_to_be_taken != 0:
+			if player.full_inventory and (current_amount - amount_to_be_taken != 0 or item_to_be_taken == "house"):
 				current_dialogue_line = 'item_cap'
 			else:
 				current_dialogue_line = 'quest_completed'

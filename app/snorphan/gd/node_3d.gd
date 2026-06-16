04fb452cd34f9258ba7a65a89f0bcaa49489048c
Joyce Lin <jlin60@stuy.edu>
2026-06-16 11:41:15 -0400

    i might have rocks for brains

--- app/snorphan/gd/node_3d.gd
+++ app/snorphan/gd/node_3d.gd
@@ -118,14 +118,14 @@ func instantiate_snowmen(snowmen):
 				
 			get_tree().current_scene.add_child(snowman)
 			
-			var x = data.get('x_coord', 0)
-			var y = data.get('y_coord', 0)
-			var z = data.get('z_coord', 0)
+			var x = snowmen.get('x_coord', 0)
+			var y = snowmen.get('y_coord', 0)
+			var z = snowmen.get('z_coord', 0)
 			
 			snowman.position.x = x
 			snowman.position.y = y
 			snowman.position.z = z
 			
-			snowman.rotation.y = data.get('y_rot', 0)
+			snowman.rotation.y = snowmen.get('y_rot', 0)
 			
 			snowman.set_up(data)
